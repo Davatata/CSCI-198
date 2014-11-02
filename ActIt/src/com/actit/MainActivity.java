e2997045d0f4afa566c68debd8c0deb81b949d46
David <davatata@gmail.com>
2014-11-01 17:19:58 -0700

    fb_event_update
    onResume: AppEventsLogger.activateApp
    onPause: AppEventsLogger.deactivateApp

--- ActIt/src/com/actit/MainActivity.java
+++ ActIt/src/com/actit/MainActivity.java
@@ -1,5 +1,7 @@
 package com.actit;
 
+import com.facebook.AppEventsLogger;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -17,6 +19,23 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	protected void onResume() {
+	  super.onResume();
+
+	  // Logs 'install' and 'app activate' App Events.
+	  String appId = getResources().getString(R.string.facebook_app_id);
+	  AppEventsLogger.activateApp(getApplicationContext(), appId);
+	}
+	
+	@Override
+	protected void onPause() {
+	  super.onPause();
+
+	  // Logs 'app deactivate' App Event.
+	  AppEventsLogger.deactivateApp(this);
 	}
 
 }
