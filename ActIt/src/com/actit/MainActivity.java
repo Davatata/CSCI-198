41162274bcd97fb1092e67eaf3eaa6d568a606ae
David <davatata@gmail.com>
2014-11-06 17:55:15 -0800

    Fragment class
    Added fragment class to handle facebook sessions
    Changed FB button: displays "Log out" when logged in

--- ActIt/src/com/actit/MainActivity.java
+++ ActIt/src/com/actit/MainActivity.java
@@ -1,17 +1,39 @@
 package com.actit;
 
-import com.facebook.AppEventsLogger;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
 
+import com.facebook.AppEventsLogger;
+import android.support.v4.app.Fragment;
+import com.fragments.MainFragment;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity  {
+	private MainFragment mainFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+
+	    if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        mainFragment = new MainFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        mainFragment = (MainFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
 	}
 
 	@Override
