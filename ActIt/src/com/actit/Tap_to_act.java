743b0e842ae1ee1d9d889b28f12d59ff3c90ba33
David <davatata@gmail.com>
2014-12-12 00:04:07 -0800

    Tap_to_act Camera
    Ready/Continue opens the front camera
    Pause stops camera and time
    Done will finish activity and go to last activity

--- ActIt/src/com/actit/Tap_to_act.java
+++ ActIt/src/com/actit/Tap_to_act.java
@@ -2,23 +2,41 @@ package com.actit;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Tap_to_act extends Activity {
+public class Tap_to_act extends Activity implements SurfaceHolder.Callback{
 	private Button start_new_game;
+	private Button end_game;
 	private EditText mycounter;
 	private CountDownTimer cdTimer;
 	private long total = 60000;
+	
+	int defaultCameraId;
+	Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    PictureCallback rawCallback;
+    ShutterCallback shutterCallback;
+    PictureCallback jpegCallback;
+    private final String tag = "VideoServer";	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +44,22 @@ public class Tap_to_act extends Activity {
 		setContentView(R.layout.activity_tap_to_act);
 		ActionBar actionBar = getActionBar();
 		actionBar.hide();
+
+	
+		
+		end_game = (Button) findViewById(R.id.button1);
+		end_game.setOnClickListener(new OnClickListener() {
+			public void onClick(View view){
+				finish();
+			}
+		});
 		
 		start_new_game = (Button) findViewById(R.id.create_game);
 		start_new_game.setOnClickListener(new OnClickListener() {
 			public void onClick(View view){
 				
 				if(start_new_game.getText().equals("Ready!") || start_new_game.getText().equals("Continue")){
+					start_camera();
 					start_new_game.setText("Pause");
 					cdTimer = new CountDownTimer(total, 1000) {
 
@@ -53,12 +81,25 @@ public class Tap_to_act extends Activity {
 					  }.start();
 				}
 				else if(start_new_game.getText().equals("Pause")){
+					stop_camera();
 					start_new_game.setText("Continue");
 					cdTimer.cancel();
 				}
 				
 			}
 		});
+		
+		surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        rawCallback = new PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Log.d("Log", "onPictureTaken - raw");
+            }
+        };
+        
+        
 		
 		mycounter = (EditText) findViewById(R.id.editText2);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -89,6 +130,7 @@ public class Tap_to_act extends Activity {
 	@Override
 	protected void onResume() {
 	  super.onResume();
+	  start_camera();
 	  //start_new_game.performClick();
 	}
 	
@@ -99,4 +141,74 @@ public class Tap_to_act extends Activity {
 	  cdTimer.cancel();
 	}
 
+	// When back is pressed from Settings, hide app
+	@Override public void onBackPressed(){ 
+		moveTaskToBack(true); 
+	}
+	
+	private void start_camera()
+    {
+		
+        try{
+            camera = Camera.open(CameraInfo.CAMERA_FACING_FRONT);
+        }catch(RuntimeException e){
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+        
+        Camera.Parameters param;
+        param = camera.getParameters();
+        //modify parameter
+        param.setPreviewFrameRate(20);
+        param.setPreviewSize(1280, 720);
+        camera.setParameters(param);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+            //camera.takePicture(shutter, raw, jpeg)
+        } catch (Exception e) {
+            Log.e(tag, "init_camera: " + e);
+            return;
+        }
+        camera.setDisplayOrientation(90);
+    }
+	
+	private void stop_camera()
+    {
+        camera.stopPreview();
+        camera.release();
+    }
+	
+	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // TODO Auto-generated method stub
+    }
+    
+    // Swap camera but unimplemented
+    public void swapCamera(){
+    	camera.stopPreview();
+        camera.release();
+    	int numberOfCameras = camera.getNumberOfCameras();
+    	
+    	if(numberOfCameras > 1){
+    		CameraInfo cameraInfo = new CameraInfo();
+    		for (int i = 0; i < numberOfCameras; i++) {
+                Camera.getCameraInfo(i, cameraInfo);
+                if (cameraInfo.facing == CameraInfo.CAMERA_FACING_BACK) {
+                    start_camera();
+                }
+                else if(cameraInfo.facing == CameraInfo.CAMERA_FACING_FRONT)
+                	start_camera();
+            }
+    	}
+    	
+    }
+    
 }
