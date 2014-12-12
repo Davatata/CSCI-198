10f0077c42e0ee354ff64bb89e12f080b7b21210
David <davatata@gmail.com>
2014-12-11 18:40:34 -0800

    Tap_to_act actionbar
    Settings button works properly

--- ActIt/src/com/actit/Tap_to_act.java
+++ ActIt/src/com/actit/Tap_to_act.java
@@ -2,7 +2,10 @@ package com.actit;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Tap_to_act extends Activity {
 
@@ -17,6 +20,21 @@ public class Tap_to_act extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tap_to_act, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_settings:
+	        	Intent intent = new Intent(this, MainActivity.class);
+        		startActivity(intent);
+	        case android.R.id.home:
+	            NavUtils.navigateUpFromSameTask(this);
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 
 }
