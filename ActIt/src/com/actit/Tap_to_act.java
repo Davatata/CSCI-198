8c22086bc005190eb018f9cace035f7b04365bb9
David <davatata@gmail.com>
2014-12-11 18:46:10 -0800

    Tap_to_act, actionbar
    Added the home up button

--- ActIt/src/com/actit/Tap_to_act.java
+++ ActIt/src/com/actit/Tap_to_act.java
@@ -13,6 +13,8 @@ public class Tap_to_act extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tap_to_act);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
